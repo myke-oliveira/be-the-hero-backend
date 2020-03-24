aa6626faa7455062d382ba3d54aaf478e81e3c9d
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-03-24 19:13:14 -0300

    JSON Response example

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.send('Hello World.');
+    return response.json({
+        evento: 'Semana Omnistack 11.0',
+        aluno: 'Myke Albuquerque Pinto de Oliveira'
+    });
 });
 
 app.listen(3333);
