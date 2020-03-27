a89071cbf0e7f6c4d7dc764a709deab0e9c49dd5
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-03-27 16:54:23 -0300

    Accessing query

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Metodos HTTP:
+ * 
+ * GET:    Buscar uma informação do back-end.
+ * POST:   Criar uma informação no back-end.
+ * PUT:    Alterar uma informação no back-end.
+ * DELETE: Deletar uma informação no backend.
+ */
+
+app.get('/users', (request, response) => {
+
+    const params = request.query;
+
+    console.log(params);
+
+
     return response.json({
         event: 'Semana Omnistack 11.0',
         student: 'Myke Albuquerque Pinto de Oliveira'
