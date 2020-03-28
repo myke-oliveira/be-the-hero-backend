d381f54e923e3b067871193d864fc530a9c92ed3
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-03-28 00:22:10 -0300

    Backend done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors(
+    // origin: 'http://meuap.com';
+));
 app.use(express.json());
 app.use(routes);
 
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,20 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
-
 const routes = express.Router();
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController')
 
-routes.post('/ongs', async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
 
-    const id = crypto.randomBytes(4).toString('HEX');
-    
-    await connection('ongs').insert({
-        id, name, email, whatsapp, city, uf,
-    })
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
 
-    return response.json({ id });
-});
+routes.delete('/incidents/:id', IncidentController.delete);
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
+routes.get('/profile', ProfileController.index);
 
-    return response.json(ongs);
-});
+routes.post('/session', SessionController.create)
 
 module.exports = routes;
